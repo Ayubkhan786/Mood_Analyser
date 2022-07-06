0ea1c8cf1db68818d97bb57547d1aadc40d95906
your Name <mohamedabdulayubm@gmail.com>
2022-07-06 21:32:07 +0530

    [Ayub] Mood Analyser

---
+++ Mood_Analyser/MoodAnalyser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Mood_Analyser
+{
+   public class MoodAnalyser
+    {
+        public string message;
+
+        public MoodAnalyser(string msg)
+        {
+            this.message = msg;
+        }
+        public string AnalyseMood(string msg)
+        {
+            //message = Console.ReadLine();
+            if (msg.Length > 0)
+            {
+                message = msg;
+            }
+            if (message.Contains("happy", StringComparison.OrdinalIgnoreCase))
+            {
+                return "happy";
+            }
+            if (message.Contains("sad", StringComparison.OrdinalIgnoreCase))
+            {
+                return "sad";
+            }
+
+            return null;
+        }
+        public void  AnalyseAnyMood(string msg)
+        {
+            //message = Console.ReadLine();
+            if (msg.Length > 0)
+            {
+                message = msg;
+            }
+                       
+            if (message.Contains("sad", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("sad");
+            }
+            else
+            {
+                Console.WriteLine("happy");
+            }
+        }
+
+   }
+}
+
